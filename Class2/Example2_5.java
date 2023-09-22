b91bd88c520650a610f17b29c4efb01461d7312b
bcepmnbwwg <78633585+bcepmnbwwg@users.noreply.github.com>
2023-09-22 21:10:31 +0800

    Update Example2_5.java

--- Class2/Example2_5.java
+++ Class2/Example2_5.java
@@ -20,7 +20,7 @@ public class Example2_5 {
                 d = {10, 20, 30, 40, 50, 60};
         // arraycopy() 利用循环把一个数组的元素的值赋给另一个数组中元素
         System.arraycopy(a, 0, b, 0, a.length);
-        System.arraycopy(c, 1, d, 1, c.length - 1);
+        System.arraycopy(c, 2, d, 2, c.length - 3);
 
         System.out.println("数组a的各个元素中的值:");
         System.out.println(Arrays.toString(a));
@@ -28,7 +28,7 @@ public class Example2_5 {
         System.out.println(Arrays.toString(b));
         System.out.println("数组c的各个元素中的值:"); // [1,2,3,4,5,6]
         System.out.println(Arrays.toString(c));
-        System.out.println("数组d的各个元素中的值:"); // [10,20,4,5,6,60]
+        System.out.println("数组d的各个元素中的值:"); // [10,20,3,4,5,,60]
         System.out.println(Arrays.toString(d));
 
     }
