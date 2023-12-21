0fa1b1654da38d1f4378e09c44d1f54dbda0ef90
yaozongbin <78633585+yaozongbin@users.noreply.github.com>
2023-12-21 10:23:58 +0800

    Add files via upload

---
+++ Example15_9.java
@@ -0,0 +1,5 @@
+package yaozongbin.co.in.Thread;
+
+public class Example15_9 {
+
+}
---
+++ Win.java
@@ -0,0 +1,13 @@
+package yaozongbin.co.in.Thread;
+
+import java.awt.event.*;
+import java.awt.*;
+import java.util.Date;
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+
+public class Win extends JFrame {
+    Thread showTime = null;
+    JTextArea text = null;
+
+}
